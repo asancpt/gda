2f8e5121f1d7882440d1508fb980550b7d53cde8
Sungpil_edison <shan@acp.kr>
2018-02-14 15:22:48 +0900

    updated install.R

---
+++ install-shotgun.R
@@ -0,0 +1,9 @@
+
+# shotgun
+.libPaths()
+install.packages("ShotgunFunctionalizeR",
+                 contriburl="http://pmx.amc.seoul.kr")
+library(ShotgunFunctionalizeR)
+# install.packages("~/asancpt/gda/lib/ShotgunFunctionalizeR_1.3-0.zip", repos = NULL, type = "win.binary")
+# install.packages('multtest')
+
---
+++ load.R
@@ -0,0 +1,13 @@
+# load ----
+ 
+library(ALL)
+library(hgu95av2)
+library(annotate)
+library(samr)
+library(som)
+library(golubEsets)
+library(MASS)
+library(class)
+library(e1071)
+library(igraph)
+library(rgl)
